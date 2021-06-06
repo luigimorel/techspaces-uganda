3e1d07513b5aa4677de45624fbfcb9efa342d145
Luigi Morel <morelmiles@gmail.com>
2021-06-06 11:35:49 +0300

    Add more reasons

--- src/components/Reasons.tsx
+++ src/components/Reasons.tsx
@@ -1,6 +1,48 @@
 const Reasons = () => {
     return (
         <div className="container">
+            {/* TO DO: Find a way of reducing the duplication */}
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="container">
+                        <i className="fas fas-bar"></i>
+                        <h4 className="text-center heading">
+                            Lorem ipsum dolor sit.
+                        </h4>
+                        <p className="leading">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aliquid, cum omnis obcaecati quia explicabo
+                            natus!
+                        </p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="container">
+                        <i className="fas fas-bar"></i>
+                        <h4 className="text-center heading">
+                            Lorem ipsum dolor sit.
+                        </h4>
+                        <p className="leading">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aliquid, cum omnis obcaecati quia explicabo
+                            natus!
+                        </p>
+                    </div>
+                </div>{' '}
+                <div className="col-md-4">
+                    <div className="container">
+                        <i className="fas fas-bar"></i>
+                        <h4 className="text-center heading">
+                            Lorem ipsum dolor sit.
+                        </h4>
+                        <p className="leading">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Aliquid, cum omnis obcaecati quia explicabo
+                            natus!
+                        </p>
+                    </div>
+                </div>
+            </div>
             <div className="row">
                 <div className="col-md-4">
                     <div className="container">
