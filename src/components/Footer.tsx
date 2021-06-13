f690ce64d93766b508cae5907e42a260bc2a6a7c
Luigi Morel <morelmiles@gmail.com>
2021-06-13 15:49:21 +0300

    Touches to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,54 +5,63 @@ const Footer = () => {
     const year = date.getFullYear();
 
     return (
-        <div className="container footer__container">
-            <div className="row mb-3">
-                <div className="col-md-4">
-                    <p className="leading ml-3b footer__para">
-                        This is the text that will be used to talk about the
-                        final web page
-                    </p>
-                </div>
-                <div className="col-md-4">
-                    <h4 className="heading ">Useful Links</h4>
-                    <ul className="list-group">
-                        <li className="list-item">
-                            <Link to="/">Home </Link>
-                        </li>
+        <div className="footer__container">
+            <div
+                style={{
+                    margin: ' 2rem 7rem',
+                    height: '2px',
+                    backgroundColor: '#fff',
+                }}
+            ></div>
+            <div className="container ">
+                <div className="row mb-3">
+                    <div className="col-md-4">
+                        <p className="leading ml-3b footer__para">
+                            This is the text that will be used to talk about the
+                            final web page
+                        </p>
+                    </div>
+                    <div className="col-md-4">
+                        <h4 className="heading ">Useful Links</h4>
+                        <ul className="list-group">
+                            <li className="list-item">
+                                <Link to="/">Home </Link>
+                            </li>
 
-                        <li className="list-item">
-                            <Link to="/resources">Resources</Link>
-                        </li>
-                        <li className="list-item">
-                            <Link to="/communities">Communities</Link>
-                        </li>
-                        <li className="list-item">
-                            <Link to="/oss">Open Source Projects</Link>
-                        </li>
-                        <li className="list-item">
-                            <Link to="/open-data">Open Data</Link>
-                        </li>
-                    </ul>
-                </div>
+                            <li className="list-item">
+                                <Link to="/resources">Resources</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to="/communities">Communities</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to="/oss">Open Source Projects</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to="/open-data">Open Data</Link>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div className="col-md-4">
-                    <h4 className="heading ">Social Links</h4>
-                    <div className="row">
-                        <i className="fas fas-facebook mb-3">
-                            <a href="#">social link</a>
-                        </i>
-                        <i className="fas fas-facebook mb-3">
-                            <a href="#">social link</a>
-                        </i>
-                        <i className="fas fas-facebook mb-3">
-                            <a href="#">social link</a>
-                        </i>
+                    <div className="col-md-4">
+                        <h4 className="heading ">Social Links</h4>
+                        <div className="row">
+                            <i className="fas fas-facebook mb-3">
+                                <a href="#">social link</a>
+                            </i>
+                            <i className="fas fas-facebook mb-3">
+                                <a href="#">social link</a>
+                            </i>
+                            <i className="fas fas-facebook mb-3">
+                                <a href="#">social link</a>
+                            </i>
+                        </div>
                     </div>
                 </div>
+                <p className="leading text-center mt-5">
+                    &copy; TechSpaces Uganda {year}
+                </p>
             </div>
-            <p className="leading text-center mt-5">
-                &copy; TechSpaces Uganda {year}
-            </p>
         </div>
     );
 };
