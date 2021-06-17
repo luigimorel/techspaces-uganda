9bc45225af519774a0c5d6dcb6ca9c0f4df138a2
Luigi Morel <morelmiles@gmail.com>
2021-06-17 23:08:45 +0300

    Add content for the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Hero = () => {
     return (
         <div className=" hero__section">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-2 col-xs-12">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Autem, quibusdam. Quam eveniet nam minima facere dolore
-                        placeat recusandae commodi aspernatur!
-                    </div>
-                    <div className="col-md-2 col-xs-12">
-                        <p className="leading">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Dolores natus illum id eum earum, laboriosam
-                            eius dignissimos fugit iure recusandae?
-                        </p>
-                    </div>
-                </div>
+            <div className="herotext__container">
+                <p className="hero__para">
+                    Ugandan <i>tech</i> ecosystem in one place
+                </p>
+                <p className="small__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nulla, aspernatur necessitatibus maiores temporibus
+                    exercitationem delectus assumenda modi laboriosam ad
+                    incidunt.
+                </p>
+                <Link to="/">
+                    <button className="hero__btn">Learn More</button>
+                </Link>
             </div>
         </div>
     );
