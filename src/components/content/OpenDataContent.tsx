b6519a6c0a9947f42b320ee6bb7d46614bdaaad5
Luigi Morel <morelmiles@gmail.com>
2021-06-20 22:39:05 +0300

    Add data to all pages

--- src/components/content/OpenDataContent.tsx
+++ src/components/content/OpenDataContent.tsx
@@ -1,4 +1,4 @@
-import placeholder from './../../assets/img-placeholder.png';
+import { data } from '../../data/openData';
 
 const OpenDataContent = () => {
     return (
@@ -6,118 +6,33 @@ const OpenDataContent = () => {
             {/* TO DO: Find a way of reducing the duplication */}
             <div className="row resources__row">
                 <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 resource mb-4">
-                    <div className="card" style={{ width: 'auto' }}>
-                        <img
-                            loading="lazy"
-                            className="card-img-top "
-                            src={placeholder}
-                            alt="Graphic content  for a resource"
-                        />
-                        <div className="card-body">
-                            <p className="card-text">
-                                <a href="link" target="_blank" rel="noreferrer">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Deleniti necessitatibus,
-                                    in aut hic iste quaerat.
-                                </a>
-                            </p>
-                        </div>
-                    </div>
+                    {data.map(({ imageURL, altText, descText, index }) => {
+                        return (
+                            <div
+                                className="col-md-4 "
+                                style={{ width: 'auto' }}
+                                key={index}
+                            >
+                                <img
+                                    loading="lazy"
+                                    className="card-img-top "
+                                    src={imageURL}
+                                    alt={altText}
+                                />
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        <a
+                                            href="link"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            {descText}
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
