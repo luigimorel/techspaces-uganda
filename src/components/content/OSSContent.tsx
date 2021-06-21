21df59be45aab96a7ef7ac4888634da9025097f2
Luigi Morel <morelmiles@gmail.com>
2021-06-21 22:07:10 +0300

    Change the structure of the content pages

--- src/components/content/OSSContent.tsx
+++ src/components/content/OSSContent.tsx
@@ -1,39 +1,55 @@
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
 import { data } from '../../data/oss';
 
 const OpenDataContent = () => {
     return (
         <div className="container ">
-            {/* TO DO: Find a way of reducing the duplication */}
             <div className="row resources__row">
-                {data.map(({ imageURL, altText, descText, index }) => {
-                    return (
-                        <div className="col-md-4 resource mb-4">
-                            <div
-                                className="col-md-4 "
-                                style={{ width: 'auto' }}
-                                key={index}
-                            >
-                                <img
-                                    loading="lazy"
-                                    className="card-img-top "
-                                    src={imageURL}
-                                    alt={altText}
-                                />
-                                <div className="card-body">
-                                    <p className="card-text">
-                                        <a
-                                            href="link"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            {descText}
-                                        </a>
-                                    </p>
+                {data.map(
+                    ({ imageURL, heading, descText, communityLink, index }) => {
+                        return (
+                            <div className="col-md-4 resource mb-4">
+                                <div
+                                    className="col-md-4 "
+                                    style={{ width: 'auto' }}
+                                    key={index}
+                                >
+                                    <img
+                                        loading="lazy"
+                                        className="card-img-top "
+                                        src={imageURL}
+                                        alt={heading}
+                                    />
+                                    <div className="card-body">
+                                        <p className="resource__heading card-text">
+                                            {heading}
+                                            <span className="heading__span">
+                                                <a
+                                                    href={communityLink}
+                                                    rel="noreferrer"
+                                                    target="_blank"
+                                                >
+                                                    <FaExternalLinkAlt />
+                                                </a>
+                                            </span>
+                                        </p>
+
+                                        <p className="card-text">
+                                            <a
+                                                href="link"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                {descText}
+                                            </a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    },
+                )}
             </div>
         </div>
     );
