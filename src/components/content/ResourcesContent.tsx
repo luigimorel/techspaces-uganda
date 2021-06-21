d4538eab2f9cdaa29fe18122c93742ecbe12b88b
Luigi Morel <morelmiles@gmail.com>
2021-06-21 22:15:39 +0300

    Change the resource content

--- src/components/content/ResourcesContent.tsx
+++ src/components/content/ResourcesContent.tsx
@@ -1,59 +1,58 @@
-export const data = [
-    {
-        index: 1,
-        imageURL:
-            'https://res.cloudinary.com/morelmiles/image/upload/v1624300160/24daffad037f8d60d565d3e5b226425c_400x400_uci4ig.png',
-        heading: 'Google Developers Group Kampala',
-        communityLink: 'https://twitter.com',
-        descText:
-            'A community of developers that are using Google products and tech to build software',
-    },
-    {
-        index: 2,
-        imageURL:
-            'https://miro.medium.com/max/500/1*tv9pIQPhwumDnYBfCoapYg.jpeg',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
-    {
-        index: 3,
-        imageURL: 'https://www.schoolstatus.com/hubfs/Social_Media/nader.jpg',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
-    {
-        index: 4,
-        imageURL:
-            'https://yt3.ggpht.com/a/AATXAJw6goL85GOINrpbAgISieyTKQWx0d8mVLY4Eanb6g=s900-c-k-c0xffffffff-no-rj-mo',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
-    {
-        index: 5,
-        imageURL:
-            'https://cdn.hashnode.com/res/hashnode/image/upload/v1573460393072/-F1dY-wHG.jpeg?auto=compress',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
-    {
-        index: 6,
-        imageURL:
-            'https://css-tricks.com/wp-content/uploads/2018/08/image-8-1-350x350.png',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
-    {
-        index: 7,
-        imageURL:
-            'https://github.com/images/modules/site/readme/samson-goddy/Samson%20Low%20Res%20Social%202.jpg',
-        heading: 'Engineering Manager OSS at Google',
-        communityLink: 'https://twitter.com',
-        descText: 'this is the text for the first image that has been rendered',
-    },
-];
+import { data } from '../../data/resources';
+
+const OpenDataContent = () => {
+    return (
+        <div className="container ">
+            <div className="row resources__row">
+                {data.map(
+                    ({ imageURL, heading, descText, communityLink, index }) => {
+                        return (
+                            <div className="col-md-4 resource mb-4">
+                                <div
+                                    className="col-md-4 "
+                                    style={{ width: 'auto' }}
+                                    key={index}
+                                >
+                                    <img
+                                        loading="lazy"
+                                        className="card-img-top "
+                                        src={imageURL}
+                                        alt={heading}
+                                    />
+                                    <div className="card-body">
+                                        <p className="resource__heading card-text">
+                                            {heading}
+                                            <span className="heading__span">
+                                                <a
+                                                    href={communityLink}
+                                                    rel="noreferrer"
+                                                    target="_blank"
+                                                >
+                                                    <FaExternalLinkAlt color="red" />
+                                                </a>
+                                            </span>
+                                        </p>
+
+                                        <p className="card-text">
+                                            <a
+                                                href="link"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                {descText}
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    },
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default OpenDataContent;
