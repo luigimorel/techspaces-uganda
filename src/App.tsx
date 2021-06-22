f0caf0c31d237216cf45649e8c3d8f9229a70ab5
Luigi Morel <morelmiles@gmail.com>
2021-06-22 21:28:32 +0300

    Add react helmet to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import { Route } from 'react-router';
+import { Helmet } from 'react-helmet';
+
 import Communities from './pages/Communities';
 import Home from './pages/Home';
 import OpenData from './pages/OpenData';
 import OpenSource from './pages/OpenSource';
 import Resources from './pages/Resources';
+
+<Helmet>
+    <title>Techspaces Uganda | Home</title>
+    <meta name="title" content="Techspaces Uganda" />
+    <meta
+        name="keywords"
+        content="community, open source, developers, kampala, uganda, web development, "
+    />
+    <meta name="robots" content="index, follow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="revisit-after" content="1 days" />
+    <meta
+        name="description"
+        content="Techspaces Uganda is a tech community led initiative with an aim of congregating information about the tech ecosystem in Uganda in one place "
+    />
+</Helmet>;
 
 const App = () => {
     return (
