2305123a6f94ce4f303baf847c7445c2f14252e5
Luigi Morel <morelmiles@gmail.com>
2021-06-22 21:32:15 +0300

    Add react helmet to resources page

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,6 +1,25 @@
+import { Helmet } from 'react-helmet';
+
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import ResourcesContent from '../components/content/ResourcesContent';
+
+<Helmet>
+    <title>Techspaces Uganda | Resources</title>
+    <meta name="title" content="Techspaces Uganda" />
+    <meta
+        name="keywords"
+        content="community, open source, developers, kampala, uganda, web development, "
+    />
+    <meta name="robots" content="index, follow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+    <meta name="revisit-after" content="1 days" />
+    <meta
+        name="description"
+        content="Techspaces Uganda is a tech community led initiative with an aim of congregating information about the tech ecosystem in Uganda in one place "
+    />
+</Helmet>;
 
 const Resources = () => {
     return (
