09c28632ef8d72fbd9ebe7ca88f557802a4ac770
Luigi Morel <morelmiles@gmail.com>
2021-08-13 08:03:18 +0300

    Refactor the herpo section images

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -1,34 +1,32 @@
 const ImageSection = () => {
+    const images = [
+        {
+            src: 'https://res.cloudinary.com/morelmiles/image/upload/v1624127126/Screenshot_2021-06-19_212457_hqgov0.jpg',
+            alt: 'Luigi Morel speaking infront of a panel',
+        },
+        {
+            src: 'https://res.cloudinary.com/morelmiles/image/upload/v1628783663/wordcamp_g7140o.jpg',
+            alt: 'People in the Ugandan tech ecosystem',
+        },
+        {
+            src: 'https://res.cloudinary.com/morelmiles/image/upload/v1628793110/wordcamp-entebbe_hsnfgu.jpg',
+            alt: 'Lining up for a meal at Word Camp Entebe',
+        },
+    ];
     return (
         <>
             <div className="container mt-2 mb-5">
                 <div className="row  hero__imgs ">
-                    <div className="col-md-4" data-testid="img">
-                        <img
-                            src="https://res.cloudinary.com/morelmiles/image/upload/v1624127126/Screenshot_2021-06-19_212457_hqgov0.jpg"
-                            loading="lazy"
-                            className="card-img-top"
-                            alt="Luigi Morel speaking infront of a panel"
-                        />
-                    </div>
-
-                    <div className="col-md-4" data-testid="img">
-                        <img
-                            src="https://res.cloudinary.com/morelmiles/image/upload/v1628783663/wordcamp_g7140o.jpg"
-                            loading="lazy"
-                            className="card-img-top"
-                            alt="People in the Ugandan tech ecosystem"
-                        />
-                    </div>
-
-                    <div className="col-md-4" data-testid="img">
-                        <img
-                            src="https://res.cloudinary.com/morelmiles/image/upload/v1628793110/wordcamp-entebbe_hsnfgu.jpg"
-                            loading="lazy"
-                            className="card-img-top"
-                            alt="Lining up for a meal at Word Camp Entebe "
-                        />
-                    </div>
+                    {images.map((image, x) => (
+                        <div className="col-md-4" key={x} data-testid="img">
+                            <img
+                                src={image.src}
+                                loading="lazy"
+                                className="card-img-top"
+                                alt={image.alt}
+                            />
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
